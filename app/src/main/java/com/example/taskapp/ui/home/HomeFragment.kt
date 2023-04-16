0bf103e3ac3e658828e557a066b2d9a70bb39b4e
aigerim054 <aiqerim.2654@gmail.com>
2023-04-16 14:58:38 +0600

    dz3 6

--- app/src/main/java/com/example/taskapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/home/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.example.taskapp.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,7 +32,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        taskAdapter = TaskAdapter()
+        taskAdapter = TaskAdapter(this::onLongClickListener, this::onUpdateClickListener)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -68,5 +71,53 @@ class HomeFragment : Fragment() {
 //    }
         val listOfTask = App.db.TaskDao()?.getAllTask()
         taskAdapter.addTasks(listOfTask as List<TaskModel>)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.sort_menu) {
+
+            val items = arrayOf("Дата", "A-z", "z-A")
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Сортировать")
+            builder.setItems(items) { _, which ->
+                when (which) {
+                    0 -> {
+                        taskAdapter.addTasks(
+                            App.db.TaskDao()?.getAllTaskByDate() as List<TaskModel>
+                        )
+                    }
+                    1 -> {
+                        taskAdapter.addTasks(
+                            App.db.TaskDao()?.getAllTaskByAlphabetAz() as List<TaskModel>
+                        )
+
+                    }
+                    2 -> {
+                        taskAdapter.addTasks(
+                            App.db.TaskDao()?.getAllTaskByAlphabetZa() as List<TaskModel>
+                        )
+                    }
+                }
+            }
+            builder.show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun onLongClickListener(pos: Int) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Удаление")
+        builder.setMessage("Вы точно хотите удалить ?")
+
+        builder.setPositiveButton("Да") { _, _ ->
+            App.db.TaskDao()?.delete(taskAdapter.getTask(pos))
+        }
+        builder.setNegativeButton("Нет") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+    private fun onUpdateClickListener(taskMode: TaskModel) {
+        findNavController().navigate(R.id.newTaskFragment, bundleOf("update" to taskMode))
     }
 }
--- app/src/main/java/com/example/taskapp/ui/home/new_task/TaskAdapter.kt
+++ app/src/main/java/com/example/taskapp/ui/home/new_task/TaskAdapter.kt
@@ -1,45 +1,76 @@
 package com.example.taskapp.ui.home.new_task
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskapp.databinding.TaskItemBinding
 import com.example.taskapp.ui.home.TaskModel
 
-class TaskAdapter : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>()  {
+class TaskAdapter(
+    private var onLongClick: (Int) -> Unit,
+    private var onUpdateClick: (TaskModel) -> Unit,
+) :
+    RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
-    private var taskList = arrayListOf<TaskModel>()
+    private var arrayTask = arrayListOf<TaskModel>()
+
+//    fun addTask(taskMode: List<TaskMode>) {
+//        arrayTask.add(taskMode)
+//        Log.e("ololo", "addTask" + taskMode.title)
+//        notifyDataSetChanged()
+//    }
 
     @SuppressLint("NotifyDataSetChanged")
     fun addTasks(list: List<TaskModel>) {
-        taskList.clear()
-        taskList.addAll(list)
+        arrayTask.clear()
+        arrayTask.addAll(list)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        return TaskViewHolder(
+    fun getTask(position: Int): TaskModel {
+        return arrayTask[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
             TaskItemBinding.inflate(
                 LayoutInflater.from(parent.context),
-                parent,
-                false
+                parent, false
             )
         )
     }
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-       holder.bind(taskList[position])
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.OnBind(
+            arrayTask[position]
+        )
     }
 
     override fun getItemCount(): Int {
-        return taskList.size
+        return arrayTask.size
+    }
+    fun deleteItemsAndNotifyAdapter(pos: Int) {
+        arrayTask.removeAt(pos)
+        notifyItemRemoved(pos)
     }
 
-    class TaskViewHolder(private val binding: TaskItemBinding):
+    inner class ViewHolder(private var binding: TaskItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(taskModel: TaskModel) {
-            binding.itemTitle.text = taskModel.title
-            binding.itemDesc.text = taskModel.desc
+
+        fun OnBind(taskMode: TaskModel) {
+            binding.itemTitle.text = taskMode.title
+            binding.itemDesc.text = taskMode.desc
+
+            itemView.setOnLongClickListener {
+                Log.e("ololo", "OnBind: $adapterPosition")
+                onLongClick(adapterPosition)
+                return@setOnLongClickListener true
+            }
+            itemView.setOnClickListener{
+                onUpdateClick(taskMode)
+            }
         }
     }
 }
